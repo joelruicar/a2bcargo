428e61b712bf32b2bc7e38e2fcba1953c32da827
Joel <chocococolu@gmail.com>
2026-04-19 01:13:24 +0200

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-
+import { defineConfig, fontProviders } from "astro/config";
 // https://astro.build/config
-export default defineConfig({});
+
+export default defineConfig({
+  output: 'static',
+   fonts: [{
+    provider: fontProviders.fontsource(),
+    name: "Koulen",
+    cssVariable: "--font-koulen"
+  },
+  {
+    provider: fontProviders.fontsource(),
+    name:"Kumbh Sans",
+    cssVariable: "--font-kumbh"
+  },
+  { 
+    provider: fontProviders.fontsource(),
+    name:"Inter",
+    cssVariable: "--font-inter"
+  }
+]
+});
