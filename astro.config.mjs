8771bc7d6d45ba2651ea39fa1d1e740860c8d58c
Joel <chocococolu@gmail.com>
2026-04-22 01:54:49 +0200

    font import changed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,5 @@
-import { defineConfig, fontProviders } from "astro/config";
-// https://astro.build/config
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
   output: 'static',
-   fonts: [{
-    provider: fontProviders.fontsource(),
-    name: "Koulen",
-    cssVariable: "--font-koulen"
-  },
-  {
-    provider: fontProviders.fontsource(),
-    name:"Kumbh Sans",
-    cssVariable: "--font-kumbh"
-  },
-  { 
-    provider: fontProviders.fontsource(),
-    name:"Inter",
-    cssVariable: "--font-inter"
-  }
-]
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,4 @@
 /// <reference path="../.astro/types.d.ts" />
+declare module "@fontsource/koulen";
+declare module "@fontsource/kumbh-sans";
+declare module "@fontsource/inter";
