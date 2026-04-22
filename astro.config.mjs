5cd90c1efd0726925658b3761d1048a37471576b
Joel <chocococolu@gmail.com>
2026-04-22 13:45:31 +0200

    SEO p1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 import { defineConfig } from "astro/config";
-
+import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   output: 'static',
+
+  site: 'https://www.a2bcargo.com',
+
+  integrations: [sitemap()],
 });
